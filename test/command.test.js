1519c0074bc5bee9e949c7d1d4d7636a75c2479e
Pat Heard <patrick.heard@cds-snc.ca>
2021-09-24 16:48:04 -0400

    fix: check if error stream exists before output (#44)
    This fixes a bug outputting error information if a command
    fails and the error does not include both stdout and stderr.

--- test/command.test.js
+++ test/command.test.js
@@ -1,6 +1,9 @@
 "use strict";
+/* eslint security/detect-child-process: "off" */
 
 const { execCommand } = require("../src/command.js");
+const proc = require("child_process");
+
 global.console = { log: jest.fn() };
 
 beforeEach(() => {
@@ -27,5 +30,18 @@ describe("execCommand", () => {
       ["🧪 \x1b[36m%s\x1b[0m\n", "cat foo"],
       ["Command failed: exit code 1"],
     ]);
+  });
+  test("only output error information that exists", () => {
+    proc.execSync = jest.fn(() => {
+      throw {
+        stderr: "well this is awkward",
+      };
+    });
+    expect(
+      execCommand({ exec: "terragrunt init", output: false }, ".")
+    ).toEqual({
+      isSuccess: false,
+      output: "well this is awkward",
+    });
   });
 });
