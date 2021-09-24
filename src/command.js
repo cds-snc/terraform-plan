6bcde435c84561bb9936c60424a1565a6c9027c6
Pat Heard <patrick.heard@cds-snc.ca>
2021-09-24 13:00:13 -0400

    fix: check if error stream exists before output
    This fixes a bug outputting error information if a command
    fails and the error does not include both stdout and stderr.

--- src/command.js
+++ src/command.js
@@ -23,7 +23,9 @@ const execCommand = (command, directory) => {
       .toString("utf8");
   } catch (error) {
     exitCode = error.status;
-    output = `${error.stdout.toString("utf8")}${error.stderr.toString("utf8")}`;
+    output = "";
+    output += error.stdout ? error.stdout.toString("utf8") : "";
+    output += error.stderr ? error.stderr.toString("utf8") : "";
     console.log(`Command failed: exit code ${exitCode}`);
   }
 
