06bdcca5797f211eec41e8f2ad12bae954ad513a
Pat Heard <patrick.heard@cds-snc.ca>
2022-10-08 02:07:09 +0000

    chore: conftest-deny format

--- test/conftest-deny/conftest-deny.tf
+++ test/conftest-deny/conftest-deny.tf
@@ -14,23 +14,23 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "github.com/cds-snc/terraform-modules//vpc"
-  name = "vpc"
+  source            = "github.com/cds-snc/terraform-modules//vpc"
+  name              = "vpc"
   billing_tag_value = "cal"
   high_availability = true
-  enable_eip = false
+  enable_eip        = false
 }
 
 module "rds" {
-  source = "github.com/cds-snc/terraform-modules//rds"
-  name = "test-rds"
+  source                  = "github.com/cds-snc/terraform-modules//rds"
+  name                    = "test-rds"
   backup_retention_period = 7
-  billing_tag_value = "cal"
-  database_name = "foo"
-  engine_version = "13.3"
-  password = "foo"
-  username = "cal"
+  billing_tag_value       = "cal"
+  database_name           = "foo"
+  engine_version          = "13.3"
+  password                = "foo"
+  username                = "cal"
   preferred_backup_window = "07:00-09:00"
-  subnet_ids = module.vpc.public_subnet_ids
-  vpc_id = module.vpc.vpc_id
+  subnet_ids              = module.vpc.public_subnet_ids
+  vpc_id                  = module.vpc.vpc_id
 }
