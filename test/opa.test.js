418a8cbd883c8b613fa9d422f4bcb21b976e6876
CalvinRodo <calvin.rodo@cds-snc.ca>
2023-03-27 23:55:13 +0000

    feat: replace rego with plain javascript
    Replace the rego that checks for output and resource changes with plain old javascript that does the samething without having to compile the rego to web assembly thus making it harder for us to validate the at outside contributors (and inside ones) are not doing anything malicous

--- test/opa.test.js
+++ test/opa.test.js
@@ -43,4 +43,40 @@ describe("getPlanChanges", () => {
       create: 0,
     });
   });
+
+  test("plan with two outputs", async () => {
+    const planJson = fs.readFileSync("./test/multiple_outputs/2_create.json");
+    const changes = await getPlanChanges(JSON.parse(planJson));
+
+    expect(changes.isChanges).toBe(true);
+    expect(changes.isDeletes).toBe(false);
+    expect(changes.resources).toEqual({
+      update: 0,
+      delete: 0,
+      create: 0,
+    });
+    expect(changes.ouputs).toEqual({
+      update: 0,
+      delete: 0,
+      create: 2,
+    });
+  });
+
+  test("plan with all output changes", async () => {
+    const planJson = fs.readFileSync("./test/multiple_outputs/all.json");
+    const changes = await getPlanChanges(JSON.parse(planJson));
+
+    expect(changes.isChanges).toBe(true);
+    expect(changes.isDeletes).toBe(false);
+    expect(changes.resources).toEqual({
+      update: 0,
+      delete: 0,
+      create: 0,
+    });
+    expect(changes.ouputs).toEqual({
+      update: 1,
+      delete: 1,
+      create: 1,
+    });
+  });
 });
