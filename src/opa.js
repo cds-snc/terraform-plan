ac0a542b33131e422d08653af6271d29f95ef9ab
Calvin Rodo <calvin.rodo@cds-snc.ca>
2023-03-28 10:22:06 -0400

    feat: replace rego with plain javascript (#177)
    * feat: replace rego with plain javascript
    
    Replace the rego that checks for output and resource changes with plain old javascript that does the samething without having to compile the rego to web assembly thus making it harder for us to validate the at outside contributors (and inside ones) are not doing anything malicous
    
    * fix: remove shellcheck
    
    * chore: remove unneeded test file

--- src/opa.js
+++ src/opa.js
@@ -1,33 +1,74 @@
-"use strict";
+const noChangesFound = (resources, outputs) => {
+  const noChangeResource = () =>
+    resources.create === 0 && resources.update === 0 && resources.delete === 0;
 
-const { loadPolicy } = require("@open-policy-agent/opa-wasm");
-const { policyWasmBase64 } = require("./policy/policy.js");
+  const noChangeOutput = () =>
+    outputs.create === 0 && outputs.update === 0 && outputs.delete === 0;
+
+  return noChangeResource() && noChangeOutput();
+};
+
+const countResourceChanges = (tfPlan, action) => {
+  const actions = tfPlan.resource_changes.filter((res) =>
+    res.change.actions.includes(action)
+  );
+  return actions.length;
+};
+
+const countOutputChanges = (tfPlan, action) => {
+  let count = 0;
+  for (let prop in tfPlan.output_changes) {
+    // This is safe as prop comes from the list of props on the object
+    // eslint-disable-next-line security/detect-object-injection
+    if (tfPlan.output_changes[prop].actions.includes(action)) {
+      count = count + 1;
+    }
+  }
+  return count;
+};
 
 /**
- * Uses ./policy/resource-changes.rego OPA policy to examine the JSON generated
- * from a tfplan file and check if there are resource changes.
  * @param {Object} planJson Terraform plan JSON object
  * @returns {Object} Resource and output changes in the tfplan
  */
 const getPlanChanges = async (planJson) => {
-  const policyWasm = Buffer.from(policyWasmBase64, "base64");
-  const policy = await loadPolicy(policyWasm);
-
-  const results = policy.evaluate(planJson);
-
   let changes;
-  if (results !== null && results.length) {
-    const noChanges = results[0].result.no_changes;
-    const resources = results[0].result.resource_changes;
-    const outputs = results[0].result.output_changes;
 
-    changes = {
-      isChanges: !noChanges,
-      isDeletes: resources.delete > 0,
-      resources: resources,
-      ouputs: outputs,
+  let resources = {
+    create: 0,
+    update: 0,
+    delete: 0,
+  };
+
+  let outputs = {
+    create: 0,
+    update: 0,
+    delete: 0,
+  };
+  if (planJson.resource_changes != undefined) {
+    resources = {
+      create: countResourceChanges(planJson, "create"),
+      update: countResourceChanges(planJson, "update"),
+      delete: countResourceChanges(planJson, "delete"),
     };
   }
+
+  if (planJson.output_changes != undefined) {
+    outputs = {
+      create: countOutputChanges(planJson, "create"),
+      update: countOutputChanges(planJson, "update"),
+      delete: countOutputChanges(planJson, "delete"),
+    };
+  }
+
+  const noChanges = noChangesFound(resources, outputs);
+
+  changes = {
+    isChanges: !noChanges,
+    isDeletes: resources.delete > 0,
+    resources: resources,
+    ouputs: outputs,
+  };
 
   return changes;
 };
