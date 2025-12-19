b4b38ad3f68745dc4f5026dd9f854aaab190eb2f
CalvinRodo <calvin.rodo@cds-snc.ca>
2025-12-18 23:10:29 -0500

    feat: give the test more data

--- test/drift-detection/drift.tf
+++ test/drift-detection/drift.tf
@@ -8,3 +8,18 @@ terraform {
   required_version = ">= 1.0.0"
 }
 
+resource "random_integer" "drift_example_one" {
+  min = 1
+  max = 100
+}
+
+resource "random_integer" "drift_example_two" {
+  min = 1
+  max = 100
+}
+
+resource "random_integer" "drift_example_three" {
+  min = 1
+  max = 100
+}
+
