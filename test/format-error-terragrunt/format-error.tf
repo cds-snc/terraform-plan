fec64442ff9642a42cf5483b35d382977a9150a8
Pat Heard <patrick.heard@cds-snc.ca>
2026-01-21 11:50:49 -0500

    fix: add missing terragrunt.hcl

--- test/format-error-terragrunt/format-error.tf
+++ test/format-error-terragrunt/format-error.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    test = {
-      source  = "hashicorp/random"
-      version = "~> 3.7.0"
-    }
-  }
-  required_version = ">= 1.0.0"
-}
-
 resource "random_id" "id" {
     byte_length = 8
 }
